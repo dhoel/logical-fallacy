47678e30e964ea8732b686b3dd13d02ef32c6aef
Dave Hoel <dkhoel@gmail.com>
2017-05-09 15:14:14 -0700

    fix linter errors in server/models.js

--- server/models.js
+++ server/models.js
@@ -1,24 +1,25 @@
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 
 const userSchema = mongoose.Schema({
-    userName: String,
-    questions: [],
-    googleId: {type:String, required:true},
-    accessToken: {type:String, required:true},
+  userName: String,
+  questions: [],
+  googleId: { type: String, required: true },
+  accessToken: { type: String, required: true },
 });
 
 const questionSchema = mongoose.Schema({
-    definition: {type:String, required:true},
-    fallacy: {type:String, required:true},
-    m: {type:Number, required:true}
-})
+  definition: { type: String, required: true },
+  fallacy: { type: String, required: true },
+  m: { type: Number, required: true },
+});
 
-userSchema.method.apiRepr = function() {
-    return this.userName;
-}
+userSchema.method.apiRepr = function () {
+  return this.userName;
+};
 
 const User = mongoose.model('User', userSchema);
 const Question = mongoose.model('Question', questionSchema);
 
-module.exports = {User, Question};
+module.exports = { User, Question };
