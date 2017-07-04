1e6fd206727dfca08990f01a8879c5dbbd89d01f
Dave Hoel <dkhoel@gmail.com>
2017-07-04 13:30:56 -0700

    adds responsive styling

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -4,10 +4,10 @@ export default function LoginPage() {
 
     return (
       <div className="login-page">
-        <h1 className="title">Logical Fallacy X</h1>
+        <h1 className="title-login">Logical Fallacy X</h1>
         <img className='login-img' src={require('../images/plato-socrates-aristotle.png')}
           alt="Socrates, Plato, and Aristotle"/>
-        <a className="btn-login" href={'/api/auth/google'}>Login with Google</a>
+        <a className="btn btn-login" href={'/api/auth/google'}>Login with Google</a>
       </div>
     );
 };
--- client/src/components/question-page.js
+++ client/src/components/question-page.js
@@ -25,32 +25,32 @@ export class QuestionPage extends React.Component {
     const question = this.props.definition;
 
     return (
-      <div>
-        <div className='user'>
-          <h3>{this.props.userName}</h3>
-        </div>
-        <div className='num-questions'>
-          <span>Questions Attempted: {this.props.totalQs}</span>
-        </div>
-        <div className='correct-answers'>
-          <span>Correct Answers: {this.props.correctQs}</span>
-        </div>
-        <div className='question'>
-          {question}
-        </div>
-        <div className='submit-answer'>
-          <form onSubmit={this.onSubmit}>
-            <input type="text" id='answer'
-              ref={ref => this.answer = ref}
-              placeholder='Answer'>
-            </input>
-            <button type='submit'
-              className='btn submit-btn'>Submit</button>
-          </form>
-        </div>
-        <div>
-          <a href='/api/auth/logout'>Log Out</a>
-        </div>
+      <div className='question-page'>
+        <nav className='topnav'>
+          <h2 className='user-greet' >Welcome {this.props.userName}</h2>
+          <span className='title-Qpage' >Logical Fallacy X</span>
+          <a className='btn btn-logout' href='/api/auth/logout'>Log Out</a>
+        </nav>
+        <section className='quiz-main'>
+          <p>Which logical fallacy is defined as:</p>
+          <div className='question'>
+            {question}
+          </div>
+          <div className='submit-answer'>
+            <form onSubmit={this.onSubmit}>
+              <input className='answer' type="text" id='answer'
+                ref={ref => this.answer = ref}
+                placeholder='Answer'>
+              </input>
+              <button type='submit'
+                className='btn btn-submit'>Submit</button>
+            </form>
+          </div>
+          <div className='totals'>
+            <span className='num num-questions' >Questions Attempted: {this.props.totalQs}</span>
+            <span className='num num-correct'>Correct Answers: {this.props.correctQs}</span>
+          </div>
+        </section>
       </div>
 
     );
