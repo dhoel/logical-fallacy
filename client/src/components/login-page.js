13f187bbe9bfff5f42c9a80f1d6b56d6b50a6eb3
Dave Hoel <dkhoel@gmail.com>
2017-07-23 12:38:28 -0700

    modifies login page resposiveness, adds og.image

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -4,10 +4,12 @@ export default function LoginPage() {
 
     return (
       <div className="login-page">
-        <h1 className="title-login">Logical Fallacy X</h1>
-        <img className='login-img' src={require('../images/plato-socrates-aristotle.png')}
-          alt="Socrates, Plato, and Aristotle"/>
-        <a className="btn btn-login" href={'/api/auth/google'}>Login with Google</a>
+        <div>
+          <h1 className="title-login">Logical Fallacy X</h1>
+          <img className="login-img" src={require('../images/plato-socrates-aristotle.png')}
+              alt="Socrates, Plato, and Aristotle"/>
+          <a className="btn btn-login" href={'/api/auth/google'}>Login with Google</a>
+        </div>
       </div>
     );
 };
